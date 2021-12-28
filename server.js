5736255945ee18aa8bde6168de5b58553cffc71e
John K <john.kellyfitness@gmail.com>
2021-12-28 17:33:05 -0500

    added design and sontinuing random function

--- server.js
+++ server.js
@@ -16,10 +16,96 @@ app.use(
 
 app.set("view engine", "ejs");
 var workout = [];
+var rworkout = [];
+app.route("/").get(function (req, res) {
+  workout = [];
+  res.render("cover");
+});
+
+app.route("/home").get(function (req, res) {
+  workout = [];
+  res.render("home");
+});
+
+app
+  .route("/random")
+  .get(function (req, res) {
+    workout = [];
+    res.render("random");
+  })
+  .post(function (req, res) {
+    const bodyPart = req.body.bodyPart;
+    const number = req.body.num;
+    var options = {
+      method: "GET",
+      url: "https://exercisedb.p.rapidapi.com/exercises",
+      headers: {
+        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        "x-rapidapi-key": config.MY_KEY,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const json = response.data;
+        console.log(bodyPart);
+        if (bodyPart === "arms") {
+          const result = json.filter(
+            (exercise) =>
+              exercise.bodyPart === "lower arms" &&
+              exercise.bodyPart === "upper arms"
+          );
+          console.log("1 " + result);
+          workout.push(result);
+        } else if (bodyPart === "legs") {
+          const result = json.filter(
+            (exercise) =>
+              exercise.bodyPart === "lower legs" &&
+              exercise.bodyPart === "upper legs" &&
+              exercise.bodyPart === "waist"
+          );
+          console.log("2 " + result);
+        } else if (bodyPart === "shoulders") {
+          const result = json.filter(
+            (exercise) =>
+              exercise.bodyPart === "shoulders" && exercise.bodyPart === "neck"
+          );
+          console.log("3 " + result);
+        } else {
+          const result = json.filter(
+            (exercise) => exercise.bodyPart === bodyPart
+          );
+          console.log("4 " + result);
+        }
+
+        workout.push(result);
+        res.redirect("/randomResults");
+      })
+      .catch(function (error) {
+        //console.log(options);
+        console.error(error);
+        res.redirect("/random");
+      });
+  });
+
+app
+  .route("/randomResults")
+  .get(function (req, res) {
+    console.log(workout);
+    res.render("randomResults", {
+      rworkout: rworkout,
+    });
+  })
+  .post(function (req, res) {
+    rworkout = [];
+    res.redirect("/random");
+  });
 
 app
   .route("/search")
   .get(function (req, res) {
+    workout = [];
     res.render("search");
   })
   .post(function (req, res) {
@@ -34,7 +120,6 @@ app
         "x-rapidapi-key": config.MY_KEY,
       },
     };
-
     axios
       .request(options)
       .then(function (response) {
