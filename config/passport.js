5fc213eca3f4952597c9326041857ed9c845c3b9
John K <john.kellyfitness@gmail.com>
2022-01-10 09:55:57 -0500

    test deploy 1

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 module.exports = function (passport) {
